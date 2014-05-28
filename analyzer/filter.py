39f4baffc2fa6be8e439d6e8abadec3423b4bde5
zeraqis <sathyaparker@gmail.com>
2014-05-28 17:01:47 +0200

    new filter

--- analyzer/filter.py
+++ analyzer/filter.py
@@ -3,7 +3,7 @@ import os
 import cPickle as pickle
 import math
 
-sub_dir = 'D:/Dev/soundcloud data/bulk'
+sub_dir = 'D:\Github\soundcloud-all\collector\data'
 track_paths = []
 comment_paths = []
 count = 0
@@ -26,14 +26,26 @@ for root, dirs, files in os.walk(sub_dir):
                         with open(comment_dir, 'rb') as comment_in:
                             comments = pickle.load(comment_in)
                             for comment in comments:
-                                if comment.timestamp:
+                                if comment.timestamp and track.user_id != comment.user_id:
                                     curr_comment += 1
                     except IOError as e:
                         print file_dir
+                        track_paths.append(file_dir)
                 if curr_comment < 4:
-                    print file_dir
+                    track_paths.append(file_dir)
                     for i in range(file_index):
                         comment_filename = str(track.id) + '_' + str(i+1) +'.comment'
                         comment_dir = os.path.join(root, comment_filename)
                         if comment_dir:
-                            print comment_dir
+                            comment_paths.append(comment_dir)
+
+try:                            
+    for dirs in track_paths:
+        os.remove(dirs)
+except:
+    print "Done"
+try:
+    for dirs in comment_paths:
+        os.remove(dirs)
+except:
+    print "No Comment"
